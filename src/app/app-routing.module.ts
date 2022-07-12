de4703216d93b3394943b319f95160f6aaf01720
alazar <alazarabraham1@gmail.com>
2022-07-11 21:30:49 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DragNDropComponent } from './drag-n-drop/drag-n-drop.component';
-import { HealthComponent } from './health/health.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { SkillsComponent } from './skills/skills.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent},
-  {path: "skills", component: SkillsComponent},
-  {path: "drag", component: DragNDropComponent},
-  {path: "health", component: HealthComponent},
-  {path: "login", component: LoginComponent}
+  {path: "skills", component: SkillsComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import { MaterialModule } from './material/material.module';
 import {DragDropModule} from '@angular/cdk/drag-drop';
-import { DragNDropComponent } from './drag-n-drop/drag-n-drop.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HealthComponent } from './health/health.component';
-import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SkillsComponent } from './skills/skills.component';
 import { SkillsAnimationComponent } from './skills-animation/skills-animation.component';
@@ -21,9 +18,6 @@ import { RouterModule } from '@angular/router';
   declarations: [
     AppComponent,
     HomeComponent,
-    DragNDropComponent,
-    HealthComponent,
-    LoginComponent,
     SkillsComponent,
     SkillsAnimationComponent
     ],
