69445ef15706427e525b1faf6357680c12479119
alazar <alazarabraham1@gmail.com>
2022-07-13 02:22:44 -0400

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent},
